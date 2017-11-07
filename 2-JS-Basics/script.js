ec108d46683e27996bb627cc0efe72fac62c9734
juanmrm <elvior2@hotmail.com>
2017-11-07 21:08:13 +0100

    [FEAT]: Working on the language basics.

---
+++ 2-JS-Basics/script.js
@@ -0,0 +1,67 @@
+// LECTURE: VARIABLES
+/*
+var name = 'John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+// LECTURE: VARIABLES 2
+/*
+var name = 'John';
+var age = 26;
+
+//console.log(name + age); // Se va a convertir a un string concatenando nombre y age
+//console.log(age + age); // Sigue siendo un number
+
+var job, isMarried;
+
+//console.log(job); // Undefined
+
+job = 'teacher';
+isMarried = false;
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+age = 'thirty six';
+job = 'driver';
+console.log(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is the last name?');
+console.log(lastName);
+
+alert(name + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried);
+*/
+
+// LECTURE: OPERATORS
+var now = 2016;
+var birthYear = now - 26;
+
+birthYear = now - 26 * 2;
+
+console.log(birthYear);
+
+var ageJohn = 30;
+var ageMark = 30;
+
+ageJohn = ageMark = (3 + 5) * 4 - 6;
+// ageJohn = ageMark = 26;
+// ageJohn = 26;
+
+ageJohn++; // ageJohn = ageJohn + 1;
+ageMark *= 2; // ageMark = ageMark * 2;
+
+console.log(ageJohn);
+console.log(ageMark);
+
+
+
+
+
+
