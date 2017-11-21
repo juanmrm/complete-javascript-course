332a788ef834374f24b65b313c8383e7f0e5ceb6
juanmrm <elvior2@hotmail.com>
2017-11-21 20:22:53 +0100

    [FEAT]: Continue working on Budgety project, some logic for budgetController.

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -8,7 +8,60 @@
 // BUDGET CONTROLLER
 var budgetController = (function () {
   
-  // Some code
+  var Expense = function(id, description, value) { // Function Constructor for Expense
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function(id, description, value) { // Function Constructor for Income
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  }
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+      
+      //[1 2 3 4 5], next ID = 6
+      //[1 2 4 6 8], next ID = 9
+      //  Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+      
+      // Create new item based on 'inc' or 'exp' type
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      // Push it into our data structure
+      data.allItems[type].push(newItem);
+
+      // Return the new element
+      return newItem;
+    },
+
+    testing: function() {
+      console.log(data);
+    }
+  };
 
 })(); // Invocamos a la funcion anonima creada
 
@@ -22,7 +75,9 @@ var UIController = (function() {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expenseContainer: '.expenses__list'
   }
   
   return {
@@ -32,6 +87,28 @@ var UIController = (function() {
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value
       }
+    },
+
+    addListItem: function(obj, type) {
+      var html, newHtml, element;
+
+      // Create HTML string with placeholder text
+      if (type === 'inc') {
+        element = DOMstrings.incomeContainer;
+        html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%descriptio%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+      } else if (type === 'exp') {
+        element = DOMstrings.expenseContainer;
+        html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%descriptio%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
+      }
+
+      // Replace the placeholder text with some actual data
+      newHtml = html.replace('%id', obj.id)
+                    .replace('%descriptio%', obj.description)
+                    .replace('%value%', obj.value);
+
+      // Insert the HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
     },
 
     getDOMstrings: function() {
@@ -47,33 +124,46 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
-  var DOM = UICtrl.getDOMstrings();
+  var setupEventListeners = function() {
+
+    const DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    
+    // Global event listener
+    document.addEventListener('keypress', function(event){
+      if (event.keyCode === 13 || event.which === 13) { // Navegadores mas antiguos usan este which
+        ctrlAddItem();  
+      }
+    });
+  }
 
   var ctrlAddItem = function() {
+
+    var input, newItem;    
     
     // 1. Get the field input data
-    var input = UICtrl.getInput();
-    console.log(input);
-
+    input = UICtrl.getInput();
+    
     // 2. Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     // 3. Add the item to the UI
+    UICtrl.addListItem(newItem, input.type);
 
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
     
   }
-  
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
-  // Global event listener
-  document.addEventListener('keypress', function(event){
-    if (event.keyCode === 13 || event.which === 13) { // Navegadores mas antiguos usan este which
-      ctrlAddItem();  
+  return {
+    init: function() {
+      console.log('Application has started');
+      setupEventListeners();
     }
-
-  });
-
+  };
 
 })(budgetController, UIController);
+
+controller.init(); // Execute all the code we need for init the application.
--- 6-Budgety/readme.txt
+++ 6-Budgety/readme.txt
@@ -1,6 +1,21 @@
 Recursos útiles: http://codingheroes.io/resources/
 
   Eventos: https://developer.mozilla.org/en-US/docs/Web/Events
-  http://keycodes.atjayjo.com/
+  KeyCodes: http://keycodes.atjayjo.com/
 
+
+Qué utilizamos en el projecto?
+  
+  - Module Pattern: Basado en IIFE (data privacy -> new scope) y Closures.
+    3 modulos: budgetController, UIController, controller
+
+  - Concepto de Funcion Inicializadora: es la unica instruccion de codigo que se está ejecutando desde fuera de los modules.
+
+  - Function Constructors
+
+  - DOM Manipulation:
+    - Add Listeners: document.querySelector(...).addEventListener(...)
+    - Retrieve Data from DOM inputs: document.querySelector(...).value
+    - Add / Remove elements from DOM:
+      insertAdjacentHTML: https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentHTML
   
