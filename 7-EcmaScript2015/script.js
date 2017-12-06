dcb27a9f227515a5c93382c0a0437893c3f8234f
juanmrm <elvior2@hotmail.com>
2017-12-06 16:59:30 +0100

    [FEAT]: Working with maps, classes and subclasses.

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -373,7 +373,7 @@ function SmithPerson(firsName, yearOfBirth, lastName, nationality) {
 var john = new SmithPerson('John', 1990); // LLamamos con 2 argumentos y asignara a undefined el resto de parametros
 var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish');
 */
-
+/*
 // ES6
 function SmithPerson(firsName, yearOfBirth, lastName = 'Smith', nationality = 'american') {
   this.firsName = firsName;
@@ -384,3 +384,148 @@ function SmithPerson(firsName, yearOfBirth, lastName = 'Smith', nationality = 'a
 
 var john = new SmithPerson('John', 1990); // LLamamos con 2 argumentos y asignara a undefined el resto de parametros
 var emily = new SmithPerson('Emily', 1983, 'Diaz', 'spanish');
+*/
+
+/////////////////////////////////////////////////
+// LECTURE: Maps
+/*
+const question = new Map();
+question.set('question', 'What is the official name of the latest major javascript version?');
+question.set(1, 'ES5');
+question.set(2, 'ES6');
+question.set(3, 'ES2015');
+question.set(4, 'ES7');
+question.set('correct', 3);
+question.set(true, 'Correct answer :D');
+question.set(false, 'Wrong, please try again!');
+
+console.log(question.get('question'));
+//console.log(question.size);
+
+if (question.has(4)) {
+  //question.delete(4);
+  console.log('Answer 4 is here');
+}
+
+// question.clear();
+
+//question.forEach((value, key) => console.log(`This is ${key}, and it's set to ${value}`));
+for (const [key, value] of question.entries()) { // Destructuring example
+  if (typeof(key) === 'number') {
+    console.log(`Answer ${key}: ${value} `);
+  }
+}
+
+const ans = parseInt(prompt('Write the correct answer'));
+console.log(question.get(ans === question.get('correct')));
+*/
+
+/////////////////////////////////////////////////
+// LECTURE: Classes
+/*
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+Person5.prototype.calcAge = function() {
+  var age = new Date().getFullYear() - this.yearOfBirth;
+  console.log(age);
+}
+
+var john5 = new Person5('John', 1990, 'teacher');
+
+// ES6
+class Person6 {
+
+  constructor(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+
+  calcAge() {
+    const age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+  }
+
+  static greeting() {
+    console.log('Hey there!');
+  }
+}
+
+const john6 = new Person6('John', 1990, 'teacher');
+Person6.greeting();
+*/
+
+/////////////////////////////////////////////////
+// LECTURE: Classes and Subclasses
+
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name;
+  this.yearOfBirth = yearOfBirth;
+  this.job = job;
+};
+
+Person5.prototype.calcAge = function() {
+  var age = new Date().getFullYear() - this.yearOfBirth;
+  console.log(age);
+}
+
+var john5 = new Person5('John', 1990, 'teacher');
+
+var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals) {
+  Person5.call(this, name, yearOfBirth, job); // Call superclass constructor 
+  this.olympicGames = olympicGames;
+  this.medals = medals;
+}
+// Asi es como hacemos que el prototype chain sea el que queremos. 
+// Person5 es la clase padre de Athlete5 que es la subclase.
+Athlete5.prototype = Object.create(Person5.prototype);
+
+Athlete5.prototype.wonMedal = function() {
+  this.medals++;
+  console.log(this.medals);
+}
+
+var johnAthlete5 = new Athlete5('John', 1990, 'teacher', 3, 10);
+johnAthlete5.calcAge();
+johnAthlete5.wonMedal();
+
+// ES6
+class Person6 {
+  
+  constructor(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+  }
+
+  calcAge() {
+    const age = new Date().getFullYear() - this.yearOfBirth;
+    console.log(age);
+  }
+  
+}
+
+class Athlete6 extends Person5 {
+ 
+  constructor(name, yearOfBirth, job, olympicGames, medals) {
+    super(name, yearOfBirth, job);
+    this.olympicGames = olympicGames;
+    this.medals = medals;
+  }
+
+  wonMedal() {
+    this.medals++;
+    console.log(this.medals);
+  }
+  
+}
+
+const johnAthlete6 = new Athlete6('John', 1990, 'teacher', 3, 10);
+johnAthlete6.calcAge();
+johnAthlete6.wonMedal();
